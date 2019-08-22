1e438c87c7e4850dbbb436d7311fd41cd654cc21
Leonid Bugaev <leonsbox@gmail.com>
2019-08-22 22:52:41 +0300

    Update build.sh

--- data/build.sh
+++ data/build.sh
@@ -20,6 +20,12 @@ if [ -z "$plugin_name" ]; then
     exit 1
 fi
 
-cp -r $TYK_GW_PATH/vendor $PLUGIN_BUILD_PATH/vendor
+# Handle if plugin has own vendor folder, and ignore error if not
+yes | cp -r $PLUGIN_BUILD_PATH/vendor $GOPATH/src | true
+
+# Move GW vendor folder to GOPATH (same step should be made during building main binaries)
+yes | cp -r $TYK_GW_PATH/vendor $GOPATH/src
+rm -rf $TYK_GW_PATH/vendor
+
 cd $PLUGIN_BUILD_PATH && \
     go build -buildmode=plugin -o $plugin_name
